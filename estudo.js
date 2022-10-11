9932b34df910d42eb84fe49c85a8b2c4b4c6d7de
PedroArthur <110056969+suassuas20@users.noreply.github.com>
2022-10-11 15:43:27 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,42 +1,69 @@
-// alert("Oi! Eu sou o Alert");
-// document.write("Estou na página.");
-// console.log("Eu estou no console.");
-let tabuada = 4;
-function alo(){
+//console.log("Console: Alô Mundo...");
+//alert("Alerta: Alô Mundo...");
 
-    let tabuada = document.getElementById("entrada").value;
+let tabuada = 8;
 
-    document.write("<h1>Tabuada do " + tabuada + "</h1>");
-    document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10));
+function meChame(){
+        document.write("Tabuada do " + tabuada + "<br>");
+        document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+        document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+        document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+        document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+        document.write(abuada  + " x 5 = " + (tabuada*5) + "<br>");
+        document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+        document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+        document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+        document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+        document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
-// for(inicio, validaçao, manutençao)
-function escreva(){
-    for(var i=1; i <= 10; i++){
-        for(var j = 1; j <=10; j++){
-            document.write(i + " x " + j + " = " + (j*i)+"<br>");
+
+
+//Aprenda o for..
+function minhaTabuada(){
+     for(var i=1;i <= 10;i++){
+
+       for(var j = 1; j <=10; j++){    
+        document.write(i + " x " + j + " = " + (i*j) + "<br>");
         }
-        document.write("<br>");
-    }
+        document.write("<br><br>");
+     }
 }
 
-function quadrado(){
-    for(var i = 2; i < 11; i++){
-        document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
-    }
+function quadrado (){
+        for(var i = 2; i < 21; i++){
+  document.write("O quadrado de " + i + " é " + (i*j) + "<br>")
+   }      
+
+ }
+
+ function total(){
+         let val = document.getElementById("valor").nodeValue;
+         let ju = document.getElementById("juros").nodeValue;
+         let resultado = (val * (1+ (ju/100)));
+         document.write("o resultado é" + resultado);
+ }
+
+function soma(){
+        let v1 = document.getElementById("v1").value;
+        let v2 = document.getElementById("v2").value;
+        let r =(v1 + v2);
+        document.getElementById("resultado").innerHTML = r;
+
+}
+//Divisão é /
+//Multiplicação é *
+function soma(){ 
+        let v1 = document.getElementById("v1").value;
+        let v2 = document.getElementById("v2").value;
+        let r =(Number (v1) + Number (v2));
+        document.getElementById("resultado").innerHTML = r;
+
+}
+function sub(){ 
+        let v1 = document.getElementById("v1").value;
+        let v2 = document.getElementById("v2").value;
+        let r =(Number (v1) + Number (v2));
+        document.getElementById("resultado").innerHTML = r;
+
 }
 
-function total(){
-     let val = document.getElementById("valor").value;
-     let ju = document.getElementById("juros").value;
-     let resultado = (val * ((ju/100))+1);
-     document.write("O total é de: " + resultado);
-}
